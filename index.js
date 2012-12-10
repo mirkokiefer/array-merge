8ea9b14e27fbb2d524663da5e7910887b65d14c7
Mirko Kiefer <mail@mirkokiefer.com>
2012-12-10 22:56:54 +0100

    rewrite, allowing n-way merges and making the code *much* simpler at the same time

--- index.js
+++ index.js
@@ -8,103 +8,64 @@ var modifierMap = function(diff) {
   }))
 }
 
-var matchPattern = function(key, patterns) {
-  var next = function(newKey) {
-    matchPattern(newKey, patterns)
-  }
-  var matchChar = function(char, pattern) {
-    if ((pattern == '*') || (char === pattern)) return true
-  }
-  for(var i in patterns) {
-    if(matchChar(key[0], patterns[i][0][0]) && matchChar(key[1], patterns[i][0][1])) {
-      return patterns[i][1](next)
-    }
-  }
-}
-
-var mergePatterns = function(result1, result2, pos1, pos2, diff1Map, diff2Map) {
-  var bothResults = {
-    push: function() {
-      result1.push.apply(result1, arguments)
-      result2.push.apply(result2, arguments)
-    }
-  }
-  var bothPos = {
-    next: function() { pos1.next(); pos2.next() }
-  }
-  return [[
-    '==', function() {
-      bothResults.push(pos1.value)
-      bothPos.next()
-    }], [
-    '++', function(next) {
-      // two additions - apply both in lexicographic order
-      if(pos1.value > pos2.value) {
-        bothResults.push(pos1.value, pos2.value)
-      } else {
-        bothResults.push(pos2.value, pos1.value)
-      }
-      bothPos.next()
-    }], [
-    '+*', function() {
-      bothResults.push(pos1.value)
-      pos1.next()
-    }], [
-    '*+', function() {
-      bothResults.push(pos2.value)
-      pos2.next()
-    }], [
-    'pp', function(next) {
-      // two pastes - apply both in lexicographic order
-      if(pos1.value > pos2.value) { next('p*'); next('*p') } else { next('*p'); next('p*') }
-    }], [
-    'p*', function() {
-      // paste - apply only to both if no conflicting paste
-      if(diff2Map[pos1.value] == '=') {
-        bothResults.push(pos1.value)
-      } else {
-        result1.push(pos1.value)
-      }
-      pos1.next()
-    }], [
-    '*p', function() {
-      if(diff1Map[pos2.value] == '=') {
-        bothResults.push(pos2.value)
-      } else {
-        result2.push(pos2.value)
-      }
-      pos2.next()
-    }], [
-    // cover cases x=, xx, -=, --
-    '**', function() { bothPos.next() }]
-  ]
-}
-
-function Position(diff) {
+function Parser(diff) {
   this.diff = diff
+  this.result = []
+  this.modifierMap = modifierMap(diff)
   this.next()
 }
-Position.prototype.next = function() {
+Parser.prototype.next = function() {
   var entry = this.diff.shift() || [0]
-  this.pattern = entry[0]
-  this.value = entry[1]
+  this.token = {
+    modifier: entry[0],
+    value: entry[1]
+  }
 }
 
-var merge = function(diff1, diff2) {
-  var diff1Map = modifierMap(diff1)
-  var diff2Map = modifierMap(diff2)
-  var result1 = []
-  var result2 = []
-  var pos1 = new Position(diff1)
-  var pos2 = new Position(diff2)
-  var patterns = mergePatterns(result1, result2, pos1, pos2, diff1Map, diff2Map)
-  while(pos1.pattern || pos2.pattern) {
-    matchPattern(pos1.pattern+pos2.pattern, patterns)
+var mergePatterns = function(parsers) {
+  var allParsersResultPush = function(value) {
+    parsers.forEach(function(each) { each.result.push(value) })
   }
-  if(_.isEqual(result1, result2)) {
-    return {result: result1}
+  return function() {
+    if (_.every(parsers, function(each) { return each.token.modifier == '=' })) {
+      allParsersResultPush(parsers[0].token.value)
+      parsers.forEach(function(each) { each.next() })
+    }
+
+    parsers.filter(function(each) { return each.token.modifier == '+' })
+      .sort(function(a, b) { return a.token.modifier > b.token.modifier })
+      .forEach(function(each) {
+        allParsersResultPush(each.token.value)
+        each.next()
+      })
+
+    parsers.filter(function(each) { return each.token.modifier == 'p' })
+      .sort(function(a, b) { return a.token.modifier > b.token.modifier })
+      .forEach(function(eachPasting) {
+        parsers.forEach(function(eachOther) {
+          if((eachOther.modifierMap[eachPasting.token.value] == '=') || (eachOther == eachPasting)) {
+            eachOther.result.push(eachPasting.token.value)
+          }
+        })
+        eachPasting.next()
+      })
+
+    if(_.some(parsers, function(each) { return _.contains(['x', '-'], each.token.modifier) })) {
+      parsers.forEach(function(each) { each.next() })
+    }
+  }
+}
+
+var merge = function() {
+  var parsers = _.map(arguments, function(each) { return new Parser(each) })
+  var applyPatterns = mergePatterns(parsers)
+  while(_.some(parsers, function(parser) { return parser.token.modifier })) {
+    applyPatterns()
+  }
+  if (_.every(parsers, function(each) { return _.isEqual(each.result, parsers[0].result) })) {
+    return {result: parsers[0].result}
   } else {
-    return {conflict: true, result: [result1, result2]}
+    return {conflict: true, result: parsers.map(function(each) { return each.result })}
   }
 }
 
